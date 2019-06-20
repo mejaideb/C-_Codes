9fdaf8b0be97899aa3cb34a707cfe5510d949d29
tavisca-jaideb <51903477+tavisca-jaideb@users.noreply.github.com>
2019-06-20 12:40:17 +0530

    Add files via upload

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new [] {11.5,12.45,44.2};
+            var result=0.0;
+            foreach(var i in grades)
+            {
+                result +=i;
+
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("Hello World!");
+        }
+    }   
+}
